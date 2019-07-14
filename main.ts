1ea0e339684f22422cc7896199b9bc17973ec629
Kishor Naik <kishor.naik011.net@gmail.com>
2019-07-14 12:37:31 +0530

    Code Commit

---
+++ main.ts
@@ -0,0 +1,40 @@
+// Let Keyword
+//console.log(numValue); // Block-scoped variable 'numValue' used before its declaration.
+let numValue = 20; // Valid Javascript
+console.log(numValue);
+
+//numValue = "Hello Javascritp"; // Compile Time Error // Type Inferance
+//console.log(numValue);
+
+let numValueTypescript: number; // typescript
+numValueTypescript = 30;
+console.log(numValueTypescript);
+
+// Compile time Error
+// Type '"Hello Typescript"' is not assignable to type 'number'.
+//numValueTypescript = "Hello Typescript";
+
+// Var Keyword
+console.log(varNumValue); // undefined
+var varNumValue = 40;
+console.log(varNumValue);
+
+//console.log(numLocalValue); // Cannot find name 'numLocalValue'
+{
+  // let Local Variable
+  let numLocalValue: number = 50;
+  console.log(numLocalValue);
+
+  // Access let Global Variable
+  console.log(numValue);
+
+  // Access Var Global Variable
+  console.log(varNumValue);
+
+  // var local Variable
+  console.log(varLocalValue);
+  var varLocalValue: number = 60;
+  console.log(varLocalValue);
+}
+//console.log(numLocalValue); // Cannot find name 'numLocalValue'
+console.log(varLocalValue);
